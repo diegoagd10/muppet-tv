f0902c4b2f76d0233a1c9b6a2b39bb46d5e38fac
diego-pc <diegoagd10@diego-pcs-MacBook-Pro.local>
2023-05-20 12:00:55 -0400

    [initial-code] Create initial code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Dimensions,
+  EmitterSubscription,
+  ScaledSize,
+} from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+function verifyIfIsLandscape(): boolean {
+  const screenDimensions: ScaledSize = Dimensions.get("window");
+  return screenDimensions.height < screenDimensions.width;
+}
 
 export default function App() {
+  const [isLandscape, setIsLandscape] = useState(verifyIfIsLandscape());
+
+  useEffect(() => {
+    const updateScreenDimensions = () => {
+      setIsLandscape(verifyIfIsLandscape());
+      StatusBar.setHidden(verifyIfIsLandscape());
+    };
+
+    const subscription: EmitterSubscription = Dimensions.addEventListener(
+      "change",
+      updateScreenDimensions
+    );
+    return () => {
+      subscription.remove();
+      StatusBar.setHidden(false);
+    };
+  }, []);
+
+  const additionalStyles = isLandscape
+    ? { marginTop: 0 }
+    : { marginTop: StatusBar.currentHeight || 0 };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, additionalStyles]}>
+      <YoutubePlayer
+        height={600}
+        play={true}
+        videoId={"6dR-Kx9ZA3s"}
+        webViewProps={{ allowsFullscreenVideo: true }}
+        forceAndroidAutoplay={true}
+        initialPlayerParams={{ controls: false, loop: true, rel: false }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
